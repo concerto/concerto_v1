72c369bbc62935393a57673a9573e98858514e72
Sir Tom <treithinger@inniogroup.com>
2008-03-21 20:57:50 +0000

    Updated Defines and changed default duration to 5 seconds

--- config.inc.php
+++ config.inc.php
@@ -6,12 +6,6 @@
  * Concerto system configuration - where people can't get to it
  */
 
-//Database Connection
-$db_host = 'studentsenate.rpi.edu';
-$db_login = 'signage';
-$db_password = '8r8hedac';
-$db_database = 'ds_temp';
-
 //Important paths
 define('ROOT_DIR', '/var/www/ds');                    //server-side path where Concerto lives
 define('COMMON_DIR', ROOT_DIR.'/common');             //server-side path to dir with resources for
@@ -20,13 +14,14 @@ define('CONTENT_DIR', ROOT_DIR.'/content/images');    //server-side path to cont
 define('TEMPLATE_DIR', ROOT_DIR.'/content/templates');//server-side path to screen templates
 
 //URLS for hyperlinks and the like
-define('SCREEN_URL', '/screen');                 //location of front-end screen program
-define('HARDWARE_URL', '/hardware');             //location of management for on-location machines
-define('ADMIN_BASE_URL','/admin');               //base URL on server for images, css, etc. for interface
-define('ADMIN_URL',ADMIN_BASE_URL.'/index.php'); //URL that can access this page (may be same as
+define('ROOT_URL', '/');                         //the root location where Concerto lives
+define('SCREEN_URL', ROOT_URL.'screen');         //location of front-end screen program
+define('HARDWARE_URL', ROOT_URL.'hardware');     //location of management for on-location machines
+define('ADMIN_BASE_URL', ROOT_URL.'admin');      //base URL on server for images, css, etc. for interface
+define('ADMIN_URL', ADMIN_BASE_URL.'/index.php');//URL that can access this page (may be same as
                                                  //  ADMIN_BASE_URL if mod_rewrite configured)
 
 //Various configuration
-define('DEFAULT_DURATION',7);                    //Default content duration, in seconds
+define('DEFAULT_DURATION', 5);                   //Default content duration, in seconds
 
 ?>
