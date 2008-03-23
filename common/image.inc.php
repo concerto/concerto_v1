a180b9c8cbd76e5cc7809261a68625c08dbd49c7
Sir Tom <treithinger@inniogroup.com>
2008-03-23 05:17:52 +0000

    Added a resize.

--- common/image.inc.php
+++ common/image.inc.php
@@ -1,23 +1,25 @@
 <?php
-function resize($filename, $new_width, $new_height){
-    list($width, $height) = getimagesize($filename);
+function resize($filename, $new_width, $new_height, $stretch = false){
+    if(!$stretch) {
+        list($width, $height) = getimagesize($filename);
 
-    $ratio = $width / $height;
-    $new_ratio = $new_width / $new_height;
+        $ratio = $width / $height;
+        $new_ratio = $new_width / $new_height;
 
-    if($ratio < $new_ratio) {
-	    $new_height = $new_height;
-	    $new_width = $new_height * $ratio;
-    } else {
-	    $new_width = $new_width;
-	    $new_height = $new_width / $ratio;
+        if($ratio < $new_ratio) {
+	        $new_height = $new_height;
+	        $new_width = $new_height * $ratio;
+        } else {
+	        $new_width = $new_width;
+	        $new_height = $new_width / $ratio;
+        }
     }
 
     $new_image = imagecreatetruecolor($new_width, $new_height);
     $image = imagecreatefromjpeg($filename) or //Read JPEG
-        $image = imagecreatefrompng($filename) or //Read PNG
-        $image = imagecreatefromgif($filename) or //Read GIF
-        $image = false;
+    $image = imagecreatefrompng($filename) or //Read PNG
+    $image = imagecreatefromgif($filename) or //Read GIF
+    $image = false;
     if($image)
 	    imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
     else
