577a2819616fc136b729b8554b8c951868538f54
Mike <ditorm@rpi.edu>
2008-03-23 03:38:43 +0000

    BrZ's addition of the dashboard page.

---
+++ admin/app/frontpage/dashboard.php
@@ -0,0 +1,8 @@
+<div class="roundcont">
+  <div class="roundtop"><img src="<? echo ADMIN_BASE_URL ?>/images/wc_tl.gif" alt="" width="6" height="6" class="corner topleft" style="display: none" /></div>
+  <div class="roundcont_main">
+    <h1>Main Cell</h1>
+    <img src="<? echo ADMIN_BASE_URL ?>/images/ds_welcome.png" alt="" />
+  </div>
+  <div class="roundbottom"><img src="<? echo ADMIN_BASE_URL ?>/images/wc_bl.gif" alt="" width="6" height="6" class="corner botleft" style="display: none" /></div>
+</div>
