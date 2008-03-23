78d14e08696ef81ff820510fe2e3fabc7e3906bd
Brian R Zaik <brian.r.zaik@gmail.com>
2008-03-23 12:28:22 +0000

    Added helpful text to the upload forms.

--- admin/app/content/_form.php
+++ admin/app/content/_form.php
@@ -6,36 +6,36 @@
 	<div>
      <table class='edit_win' cellpadding='6' cellspacing='0'>
        <tr> 
-         <td class='firstrow'><h5>Title</h5></td>
+         <td class='firstrow'><h5>Title</h5><p>Enter some words that describe this piece of content to others.</p></td>
          <td class='edit_col firstrow'>
            <input type="text" id="name" class="extended" name="content[name]" value="<?=$content->name?>">
          </td>
        </tr> 
        <tr>
-         <td><h5>Start Date</h5></td>
+         <td><h5>Start Date</h5><p>When should this piece of content start to be displayed on Concerto?</p></td>
          <td>
            <input type="text" id="width" name="content[start_time]" value="<?=$content->start_time?>">
          </td>
        </tr>
 
        <tr>
-         <td><h5>End Date</h5></td>
+         <td><h5>End Date</h5><p>When should this piece of content expire?  This might be the date of the event you are advertising.</p></td>
          <td>
            <input type="text" id="width" name="content[end_time]" value="<?=$content->end_time?>">
          </td>
        </tr>
 
        <tr>
-         <td><h5>Duration</h5></td>
+         <td><h5>Duration</h5><p>For how long should this piece of content be displayed on a screen?</p></td>
          <td>
            <input type="text" size="2" id="width" name="content[duration]" value="<?= $content->duration?$content->end_time:DEFAULT_DURATION?>"> &nbsp;seconds
          </td>
        </tr>
 
        <tr>
-         <td><h5>Feeds<h5></td>
+         <td><h5>Feeds</h5><p>In which content categories would this content fit the best?  (You may select more than one.)</p></td>
          <td>
-           Submit to feed:
+           Submit to Feed:
            <?php
            foreach ($this->feeds as $arr) {
               list($feed, $value) = $arr;
--- admin/app/content/new_image.php
+++ admin/app/content/new_image.php
@@ -1,19 +1,21 @@
 <h1>Add Image</h1>
-<h2>Fill in these details to post a new image to Concerto.</h2>
-<img src="/admin/images/graphic_icon.jpg" alt="" />
+<h2>Fill in these details to post a new graphical image to Concerto.  It could be contained within an image file (JPEG, PNG) or a PDF.</h2>
+<img src="<?= ADMIN_BASE_URL ?>/images/graphic_icon.jpg" alt="" />
 <br />
 <form enctype="multipart/form-data" method="POST" action="<?=ADMIN_URL?>/content/create">
-<?php 
-   include("_form.php");
-?>
-<table class='edit_win' style="margin-top:-18px"; cellpadding='6' cellspacing='0'>
+<br /><br /><table class='edit_win' style="margin-top:-18px"; cellpadding='6' cellspacing='0'>
   <tr>
-  <td><h5>File to Upload</h5></td>
+  <td><h5>File to Upload</h5><p><b>Specify the file you would like to upload to Concerto.</b></p></td>
   <td class="edit_col">
     <input name="content_file" class="extended" id="content_file" type="file" />
+    <br /><br />
+    <p>Accepted file types: JPEG, PNG, GIF, PDF</p>
     <input name="content[upload_type]" value="file" id="content_upload_type" type="hidden" />
   </td>
 </table>
 <br />
+<?php 
+   include("_form.php");
+?>
 <input value="Submit Content" type="submit" name="submit" />
 </form>
