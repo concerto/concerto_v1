7a05a1e166065f3d1d243df969619f5fb6a7590e
Mike <ditorm@rpi.edu>
2008-03-24 01:08:36 +0000

    contact info, fixed awaiting moderation alert

--- admin/includes/left_menu.php
+++ admin/includes/left_menu.php
@@ -22,7 +22,7 @@ src="<?php echo ADMIN_BASE_URL?>/images/conc_bluebg.gif" alt="Concerto" style=""
            <? } else { ?>
          <img src="<?= ADMIN_BASE_URL ?>/images/user_basic.gif" /><br /><br /> 
            <? } //This closes the non admin or moderator stuff 
-           echo "Welcome, " . firstName() . "!";
+           echo 'Welcome, <a href="'.ADMIN_URL.'/users/show/'.userName().'">'.firstName().'</a>!';
            ?>
            <br /><br />
            <h3><a href="<?= ADMIN_URL ?>/frontpage/logout">Logout</a></h3>
@@ -38,20 +38,22 @@ if(isLoggedIn() && $_SESSION['user']->id)
                             ' LEFT JOIN `feed` ON feed.group_id = user_group.group_id WHERE user.id = '.
                             $_SESSION['user']->id);
 if(is_array($feeds)&&count($feeds)>0) {
+        foreach($feeds as $feed) {
+           $obj = new Feed($feed['id']);
+           $modq = $obj->content_list('NULL');
+           $num = is_array($modq)?count($modq):0;
+           if($num>0)
+              $mod_feeds[]='<p><a href="'.ADMIN_URL.'/feeds/show/'.$obj->id.
+                 '">'.$obj->name.'</a> ('.$num.')</p>';
+        }
+}
+if(is_array($mod_feeds)&&count($mod_feeds)>0) {
 ?>
     <div class="alert_box">
 	   <div class="alert_box_inset">
         <div class="alert_box_padding">
           <h1>Awaiting Moderation</h1>
- <?php
-        foreach($feeds as $feed) {
-           $obj = new Feed($feed['id']);
-           $num = count($obj->content_list('NULL'));
- ?>
-          <p><a href="<?=ADMIN_URL.'/feeds/show/'.$obj->id?>"><?=$obj->name?></a> (<?=$num?>)</p>
-<?php
-        }
-?>
+<?= join(" \n ", $mod_feeds) ?>
         </div>
       </div>
     </div>
