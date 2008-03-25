3f81f10e007eb185f43ae529a15879f070b3bbbb
Brian R Zaik <brian.r.zaik@gmail.com>
2008-03-25 04:14:20 +0000

    Removed stoopid intro image from dashboard.  Add notifications to this page pronto!

--- admin/app/frontpage/dashboard.php
+++ admin/app/frontpage/dashboard.php
@@ -1,8 +1,8 @@
 <div class="roundcont">
   <div class="roundtop"><img src="<? echo ADMIN_BASE_URL ?>/images/wc_tl.gif" alt="" width="6" height="6" class="corner topleft" style="display: none" /></div>
   <div class="roundcont_main">
-    <h1>Main Cell</h1>
-    <img src="<? echo ADMIN_BASE_URL ?>/images/ds_welcome.png" alt="" />
+    <h1>Did You Know?</h1>
+    <p>You can check out the help and support center for Concerto <a href="<?= ROOT_URL ?>docs/">here</a>.  New updates will be continuously added!</p>
   </div>
   <div class="roundbottom"><img src="<? echo ADMIN_BASE_URL ?>/images/wc_bl.gif" alt="" width="6" height="6" class="corner botleft" style="display: none" /></div>
 </div>
