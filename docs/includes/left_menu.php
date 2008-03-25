a85b45fb684b26a0033d2bf167484c5a26aa8d45
Brian R Zaik <brian.r.zaik@gmail.com>
2008-03-25 04:15:08 +0000

    Removed broken links from menu - they will be re-added once I write their pages...

--- docs/includes/left_menu.php
+++ docs/includes/left_menu.php
@@ -3,7 +3,5 @@
 <div id="toc">
   <ol>
     <li><a href="page1.php">Introducing Concerto</a></li>
-    <li><a href="page2.php">System Overview</a></li>
-    <li><a href="page3.php">Control Panel</a></li>
   </ul>
 </div>
