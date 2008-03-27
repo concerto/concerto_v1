e212803c002e9cd8543e173b6968463efe31d973
Brian Michalski <bmichalski@gmail.com>
2008-03-27 18:55:30 +0000

    Index cleaned up

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+include('config.inc.php');
 if(isset($_GET['mac'])) {
-//header('Location: http://signage.union.rpi.edu/screen?'.$_SERVER["QUERY_STRING"]);
-header('Location: http://ds.rpitv.org/tom?'.$_SERVER["QUERY_STRING"]);
+   header('Location: '.SCREEN_URL.'?'.$_SERVER["QUERY_STRING"]);
 } else {
+   header('Location: '.ADMIN_URL);
+}
 ?>
 <head>
 <title>Concerto</title>
@@ -10,7 +12,7 @@ header('Location: http://ds.rpitv.org/tom?'.$_SERVER["QUERY_STRING"]);
 <body style="background-color:#069; height:100%">
 <div style="text-align:center;position:relative; top:25%;">
 <img src="admin/images/conc_bluebg.gif">
-<p style="color:white; font-family:sans-serif; font-style:italic; font-size:0.85em">coming soon</p>
+<p style="color:white; font-family:sans-serif; font-style:italic; font-size:0.85em">Welcome</p>
 </div>
 </body>
-<?}?>
+
