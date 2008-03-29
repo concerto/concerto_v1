5666163477d87be48e9aa5803b0cb27d01576762
Brian R Zaik <brian.r.zaik@gmail.com>
2008-03-29 18:30:17 +0000

    Updated content style submission guidelines.

--- docs/page2.php
+++ docs/page2.php
@@ -1,18 +1,25 @@
 <?php include("includes/pageheader.php") ?>  <div id="content_header">
     <h1>Style Guide for Graphical Content</h1>
-    <h2>Last Updated: 03/25/08 at 1:15 pm EST</h2>
+    <h2>Last Updated: 03/29/08 at 3:35 pm EST</h2>
   </div>
 
   <div id="maincontent">
   <!-- main content begins here -->
 <p>One of the most important elements of any Concerto display is the use of eye-catching, vivid images.  Graphics can take the form of event flyers, original artistic creations, or visual announcements.  But if you are submitting content that is graphical in nature, you'll want to pay careful attention to the size, colors, fonts, and composition of your content.</p>
-<p>Most graphical content on the average Concerto screen will appear in one or more designated "graphics" blocks on the screen.  These blocks are generally in a 4:3 aspect ratio (4" wide and 3" tall, or some multiple of that ratio).  That means that the recommended size for most graphical content should preserve these relative dimensions.  The best resolution for an image you may submit to the system is <b>1024 pixels by 768 pixels</b>, which is a common resolution of many computer monitors.  If you don't stick to a 4:3 aspect ratio, Concerto displays will take the larger dimension and shrink the entire image to fit in each graphics block.  For this reason, you should avoid creating content that is really narrow or really tall.</p>
+<p>Most graphical content on the average Concerto screen will appear in one or more designated "graphics" blocks on the screen.  These blocks are generally in a 4:3 aspect ratio (4" wide and 3" tall, or some multiple of that ratio).  That means that the recommended size for most graphical content should preserve these relative dimensions.  The best resolution for an image you may submit to the system is <b>1280 pixels by 1024 pixels</b>, which is a common resolution of many computer monitors.  If you don't stick to a 4:3 aspect ratio, Concerto displays will take the larger dimension and shrink the entire image to fit in each graphics block.  For this reason, you should avoid creating content that is really narrow or really tall.</p>
 <ul>
-<li>Emphasize the parts of the slide, such as the name of an event, your club or organization, or the time and place of an event in a large, readable font face.</li>
-<li>Leave about 10% free space on all sides of the slide for margins.  This space will likely get cut off during broadcast (see the graphic to the right).</li>
-<li>Use fonts at size 20pt or higher.  Stick to sans-serif fonts such as Arial, Helvetica, and Verdana (not serif fonts such as Times New Roman or Book Antiqua.</li>
-<li>Try to stay away from super-bright colors such as pure white, bright yellow, and hot pink.  If you have to use a bright color, use it very sparingly.</li>
-<li>Don't use copyrighted content unless you get permission beforehand from the copyright holder.  There might be an exception if you use fair-use materials.  If you can't tell if something is copyrighted or not, don't use it.</li>
-<li>Remember that each slide you submit will remain up on a display for the number of seconds you specify as the duration.  The average duration we recommend is 8 seconds per piece of content.  If that's not enough time to comunicate all the info you need to get across, consider breaking up the information into multiple slides.</li>
+<li>Emphasize in a large, readable type face the most important content on the slide, such as the name of an event, club, or organization, or the time and place of an event.</li>
+<li>Use sans-serif fonts, such as Arial, Helvetica, and Verdana (as opposed to serif fonts such as Times New Roman or Garamond).  Stick to size 20pt font or higher.</li>
+<li>Refrain from using super-bright colors such as pure white, bright yellow, and hot pink. If you must to use a bright color, use it very sparingly.</li>
+<li>Do not use copyrighted content unless you get consent from the copyright holder. If you use fair-use materials, follow the terms of the copyright. If you can't tell if something is copyrighted or not, don't use it.</li>
+<li>Remember that each slide you submit will remain up on a display for the number of seconds you specify as the duration.  If our recommended duration of 8 seconds per graphic is not enough time to communicate all of your information, consider breaking up the information into multiple slides.</li>
+</ul>
+<h1>Templates</h1>
+<p>Creating content can be done using many different programs: Microsoft Powerpoint, Paint Shop Pro, Microsoft Word, and others.  We recommend, however, that if you have access to an Adobe content creation application, such as Photoshop, Illustrator, or InDesign, that you use of those to create new graphical content.  Below you will find a set of templates for these applications.  If you do not have any of these programs, five machines in the north portion of the VCC lobby the entire Adobe Creative Suite 3 installed.  Two are adjacent to the two flatbed scanners and three more are opposite those machines.</p>
+<p>If you will be using content you have not created yourself, we recommend using the templates to fit or crop the content to Concerto-friendly content.</p>
+<ul>
+<li><a href="http://myrpi.org/conctemplates/Concerto_PhotoshopTemplate.psd">Photoshop Template</a></li>
+<li><a href="http://myrpi.org/conctemplates/Concerto_IllustratorTemplate.ai">Illustrator Template</a></li>
+<li><a href="http://myrpi.org/conctemplates/Concerto_InDesignTemplate.indd">InDesign Template</a></li>
 </ul>
 <?php include("includes/pagefooter.php") ?>
