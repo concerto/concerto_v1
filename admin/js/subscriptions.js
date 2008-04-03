6452392661e4d0e5cd2d9c7a085ad6842bcfc202
Mike <ditorm@rpi.edu>
2008-04-03 19:46:20 +0000

    Fixed up subscriptions page - feed in list, default weight now .66

--- admin/js/subscriptions.js
+++ admin/js/subscriptions.js
@@ -18,14 +18,13 @@ function addPos(field, url) {
    var feed=add.options[add.selectedIndex];
    e.setAttribute('id','pos_'+field+'_'+feed.value);
 
-
    e.innerHTML=
 '<select name="content[freq]['+field+']['+feed.value+']">'+
-'<option value="0" SELECTED>Never</option><option value=".33">Sometimes</option><option value=".66">Moderately</option><option value="1.00">Very Often</option>'+
+'<option value="0">Never</option><option value=".33">Sometimes</option><option value=".66" SELECTED>Moderately</option><option value="1.00">Very Often</option>'+
 '</select> display content from <a href="/admin/feeds/show/'+feed.value+'">'+feed.text+'</a> '+
 '(<a href="#" onclick="removePos('+field+','+feed.value+',\''+feed.text+'\'); return false;">remove</a>)';
 
-   add.parentNode.parentNode.insertBefore(e,add.parentNode);   
+   add.parentNode.parentNode.getElementsByTagName('ul')[0].appendChild(e);   
 
    add.options[add.selectedIndex]=null;
 }
