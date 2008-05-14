c3acbea5554e213bb60eae7265ebea2ad56de4f8
Brian R Zaik <brian.r.zaik@gmail.com>
2008-05-14 19:34:54 +0000

    Something I'm sure you're all biting your fingernails over -- the front log in page -- has received significant changes.  Whew!

--- admin/app/frontpage/index.php
+++ admin/app/frontpage/index.php
@@ -1,3 +1,17 @@
-<h2>Have an RCS ID and password?  If so, you can log into Concerto to the left!</h2>
-<img src="<?= ADMIN_BASE_URL ?>images/present.jpg" alt="Introducing Concerto" />
-<p>Concerto introduces an entirely new way of reaching out to the rest of RPI's campus.  With Concerto, you can communicate events, announcements, and other information to campus through a variety of digital means.  Several high-resolution displays, similar to the one above, have been deployed in high-traffic areas of RPI's Troy campus.  Integration with existing services, such as dining halls and the Institute Events Calendar, is also dead simple - Concerto simply retrieves content from them.  Content on each screen can be controlled by the people who are most directly connected to that display.</p>
+<div class="roundcont">
+  <div class="roundtop"><img src="<? echo ADMIN_BASE_URL ?>images/wc_tl.gif" alt="" width="6" height="6" class="corner topleft" style="display: none" /></div>
+  <div class="roundcont_main">
+    <div style="float:left; width:86px; height:71px;"><img width="70" height="70" src="<?= ADMIN_BASE_URL ?>images/rpi_seal.gif" alt="RPI Seal" /></div>
+    <h1>Have an RCS ID and password?  If so, you can log into Concerto to the left!</h1>
+    <p>Concerto is 100% free to use for all current members of the campus community at <a href="http://rpi.edu">Rensselaer Polytechnic Institute</a>.</p>
+    <div style="clear:both;height:1px;"></div>
+  </div>
+  <div class="roundbottom"><img src="<? echo ADMIN_BASE_URL ?>images/wc_bl.gif" alt="" width="6" height="6" class="corner botleft" style="display: none" /></div>
+</div>
+<h2></h2>
+<div id="fp_present">
+  <br /><br /><h1>Digital Signage Powered by Community</h1>
+  <p>Concerto introduces an entirely new way of reaching out to the rest of RPI's campus.  With Concerto, you can communicate events, announcements, and other important information to campus through a variety of digital means.</p>
+  <p>A growing number of high-resolution displays have been deployed in high-traffic areas of RPI's Troy campus.  These screens can pull from the collection of content categories, also known as <i>feeds</i>, already in the system and open to submissions from the community.</p>
+  <p>Integration with existing services, such as dining halls and the Institute Events Calendar, is also dead simple - Concerto simply retrieves content from them.  Content on each screen can be controlled by the people who are most directly connected to that display.</p>
+</div>
