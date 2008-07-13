a0f4c532545dfe361242045aff6a1ce4d9772384
Brian R Zaik <brian.r.zaik@gmail.com>
2008-07-13 00:47:15 +0000

    Added feedback form styling.

--- admin/app/pages/_feedback.php
+++ admin/app/pages/_feedback.php
@@ -41,30 +41,35 @@ $(document).ready(function() {
 });
 </script>
 
-<h3>Was this information helpful?</h3>
-
-<form id="feedback_form" method="POST" action="<?=ADMIN_URL?>/pages/feedback">
-  <p id="feedback_helpful">
-    <input type="radio" name="helpful" value="1" id="feedback_yes">
-    <label for="helpful" id="feedback_yes_lbl">Yes</label>
-    </input>
-    <input type="radio" name="helpful" value="0" id="feedback_no"/>
-    <label for="helpful" id="feedback_no_lbl">No</label>
-  </p>
-  <p id="feedback_msg">
-    <label for="message">What can we do to improve this page?</label><br/>
-    <textarea cols="40" rows="4" name="message"></textarea><br/>
-<? if(isLoggedIn()) { ?>
-    <input type="hidden" name="email" value="" />
-    <input type="hidden" name="human" value="person" /><br/>
-<? } else { ?>
-    <label for="email">Your Email: <em>(optional)</em></label><br />
-    <input type="text" name="email" value="" /><br/>
-    <label for="human">Are you a person or a robot?: <em>(please type 'person' or 'robot')</em></label><br />
-    <input type="text" name="human" value="" /><br/>
-<? } ?>
-
-    <input type="hidden" name="page_id" value="<?=$this->page['id']?>" />
-    <input value="Submit Feedback" type="submit" name="submit" />
-  </p>
-</form>
+<div id="feedback_box">
+	<div id="feedback_inner">
+		<h1>Was this information helpful?</h1>
+		<h2>Your input can help us improve the Concerto Support Center.</h2>
+		
+		<form id="feedback_form" method="POST" action="<?=ADMIN_URL?>/pages/feedback">
+			<p id="feedback_helpful">
+				<input type="radio" name="helpful" value="1" id="feedback_yes">
+				<label for="helpful" id="feedback_yes_lbl">Yes</label>
+				</input>
+				<input type="radio" name="helpful" value="0" id="feedback_no"/>
+				<label for="helpful" id="feedback_no_lbl">No</label>
+			</p>
+			<p id="feedback_msg">
+				<label for="message">What can we do to improve this page?</label><br/>
+				<textarea cols="40" rows="4" name="message"></textarea><br/>
+		<? if(isLoggedIn()) { ?>
+				<input type="hidden" name="email" value="" />
+				<input type="hidden" name="human" value="person" /><br/>
+		<? } else { ?>
+				<label for="email">Your Email: <em>(optional)</em></label><br />
+				<input type="text" name="email" value="" /><br/>
+				<label for="human">Are you a person or a robot?: <em>(please type 'person' or 'robot')</em></label><br />
+				<input type="text" name="human" value="" /><br/>
+		<? } ?>
+		
+				<input type="hidden" name="page_id" value="<?=$this->page['id']?>" />
+				<input value="Submit Feedback" type="submit" name="submit" />
+			</p>
+		</form>
+	</div>
+</div>
