1a1c5d406fc4d71249165f2532c078eaa01a661b
Mike <ditorm@rpi.edu>
2008-09-02 19:52:39 +0000

    Removed the old docs area, which has been replaced by Pages long ago.

--- docs/includes/left_menu.php
+++
@@ -1,8 +0,0 @@
-<br /><center><a href="http://signage.union.rpi.edu/docs/"><img src="images/conc_support.gif" alt="Concerto Support" style="" border="0" /></a></center>
-<h1><a href="http://signage.union.rpi.edu/admin">Control Panel</a></h1>
-<div id="toc">
-  <ol>
-    <li><a href="page1.php">Introducing Concerto</a></li>
-    <li><a href="page2.php">Style Guide for Graphical Content</a></li>
-  </ul>
-</div>
