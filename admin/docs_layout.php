52ff4c967359d328dcb732dec35751cce4779c99
Brian Michalski <bmichalski@gmail.com>
2008-09-03 02:22:51 +0000

    Google Analytics will help us analyze users.

--- admin/docs_layout.php
+++ admin/docs_layout.php
@@ -66,6 +66,14 @@ if(is_array($this->menu_links))
       <p><a href="<?= ADMIN_URL ?>">Control Panel</a> | <a href="http://myrpi.org/webtech/list_posts">Web Tech Blog</a> | Contact Support: <a href="mailto:concerto@union.rpi.edu">concerto@union.rpi.edu</a></p>
     </div>
   </div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-248819-8");
+pageTracker._trackPageview();
+</script>
 </body>
 </html>
 
--- admin/ds_layout.php
+++ admin/ds_layout.php
@@ -65,7 +65,14 @@
     <p><a href="<?= ADMIN_URL ?>/pages/show/docs/">Support Center</a> | <a href="http://myrpi.org/webtech/list_posts">Web Tech Blog</a> | Contact Support: <a href="mailto:concerto@union.rpi.edu">concerto@union.rpi.edu</a></p>
   </div>
 </div>
-
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-248819-8");
+pageTracker._trackPageview();
+</script>
 </body>
 </html>
 
