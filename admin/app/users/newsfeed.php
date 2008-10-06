0c234efec24d260e15d366af08c95bef2bf6a221
Brian R Zaik <brian.r.zaik@gmail.com>
2008-10-06 14:58:48 +0000

    Added seek buttons for page control on user's newsfeed page.

--- admin/app/users/newsfeed.php
+++ admin/app/users/newsfeed.php
@@ -19,16 +19,20 @@ $(function()
 <div class="roundcont newsfeed">
   <div class="roundtop"><span class="rt"><img src="<? echo ADMIN_BASE_URL ?>/images/blsp.gif" height="6" width="1" alt="" /></span></div>
   <div class="roundcont_main">
-    <div style="text-align:right; float:right; width:200px;">
+    <div style="text-align:right; float:right; width:240px;">
     <? $num = count($this->notifications) ?>
-   
+   	<? if($this->page > 0) { ?>
+   		<a href="<?= ADMIN_URL ?>/users/newsfeed/<?= userName() ?>/0"><span class="buttonsel"><div class="buttonleft"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_left.gif" border="0" alt="" /></div><div class="buttonmid"><div class="buttonmid_padding">|<</div></div><div class="buttonright" style="width:10px; padding-right:12px;"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_right.gif" border="0" alt="" /></div></span></a>
+   	<? } ?>
     <? if($this->page > 0) {?>
-			<a href="<?= ADMIN_URL ?>/users/newsfeed/<?= userName() ?>/<?= $this->page - 1?>"><span class="buttonsel"><div class="buttonleft"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_left.gif" border="0" alt="" /></div><div class="buttonmid"><div class="buttonmid_padding">Previous Page</div></div><div class="buttonright" style="width:10px;"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_right.gif" border="0" alt="" /></div></span></a>
+			<a href="<?= ADMIN_URL ?>/users/newsfeed/<?= userName() ?>/<?= $this->page - 1?>"><span class="buttonsel"><div class="buttonleft"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_left.gif" border="0" alt="" /></div><div class="buttonmid"><div class="buttonmid_padding"><<</div></div><div class="buttonright" style="width:10px; padding-right:12px;"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_right.gif" border="0" alt="" /></div></span></a>
     <? } ?>
     <? if($num>1) {?>
-			<img src="<?= ADMIN_BASE_URL ?>/images/blsp.gif" width="5" height="1" alt="" />
-			<a href="<?= ADMIN_URL ?>/users/newsfeed/<?= userName() ?>/<?= $this->page + 1?>"><span class="buttonsel"><div class="buttonleft"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_left.gif" border="0" alt="" /></div><div class="buttonmid"><div class="buttonmid_padding">Next Page</div></div><div class="buttonright" style="width:10px;"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_right.gif" border="0" alt="" /></div></span></a>
+			<a href="<?= ADMIN_URL ?>/users/newsfeed/<?= userName() ?>/<?= $this->page + 1?>"><span class="buttonsel"><div class="buttonleft"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_left.gif" border="0" alt="" /></div><div class="buttonmid"><div class="buttonmid_padding">>></div></div><div class="buttonright" style="width:10px; padding-right:12px;"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_right.gif" border="0" alt="" /></div></span></a>
     <? } ?>
+   	<? if($this->page < $num) { ?>
+   		<a href="<?= ADMIN_URL ?>/users/newsfeed/<?= userName() ?>/<?= floor($this->notification_count / $num) ?>"><span class="buttonsel"><div class="buttonleft"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_left.gif" border="0" alt="" /></div><div class="buttonmid"><div class="buttonmid_padding">>|</div></div><div class="buttonright" style="width:10px; padding-right:12px;"><img src="<?= ADMIN_BASE_URL ?>/images/buttonsel_right.gif" border="0" alt="" /></div></span></a>
+   	<? } ?>
     </div>
 <? if ($num>0) { ?>
     <h2>Showing items <?= $this->start+1 ?> to <?= $this->start + count($this->notifications) ?> of <?= $this->notification_count ?> total notifications</h2>
