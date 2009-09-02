368222ea30295502253440ecb7521bc894995565
Brian R Zaik <brian.r.zaik@gmail.com>
2009-09-02 02:11:32 +0000

    Removed old crap from the public page of old, and changed order of the page titles for SEO and consistency with our other web apps.

--- admin/ds_layout.php
+++ admin/ds_layout.php
@@ -28,7 +28,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title><?php echo join(' - ',array('Concerto Panel', $this->getTitle()));?></title>
+<title><?php echo join(' - ',array($this->getTitle(), 'Concerto Panel'));?></title>
 <link rel="stylesheet" type="text/css" href="<?=ADMIN_BASE_URL?>css/admin_new.css" />
 <link rel="stylesheet" type="text/css" href="<?=ADMIN_BASE_URL?>css/ui.lightbox.css" />
 <link rel="stylesheet" type="text/css" href="<?=ADMIN_BASE_URL?>css/ui.tablesort.css" />
