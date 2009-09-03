a4025004265091b56d5103efa032ee97e19aac0d
Brian Michalski <bmichalski@gmail.com>
2009-09-03 03:48:37 +0000

    Fixing Dashboard link

--- admin/includes/menu_tabs.php
+++ admin/includes/menu_tabs.php
@@ -31,7 +31,7 @@
 	if(isLoggedIn()) { //We will change this!
 	?>
 	  <ul>
-	    <li><a href="<?= ADMIN_URL ?>" title="Dashboard"><span>Dashboard</span></a></li>
+	    <li><a href="<?= ADMIN_URL ?>/frontpage/dashboard" title="Dashboard"><span>Dashboard</span></a></li>
 	    <li><a style="margin-left:12px;" href="<?= ADMIN_URL ?>/content/new" title="Add new content to the system"><span>Add Content</span></a></li>
 	    <li><a href="<?= ADMIN_URL ?>/browse/" title="Browse all content in the system, sorted by feed"><span>Browse Content</span></a></li>
 	    <li><a style="margin-left:12px" href="<?= ADMIN_URL ?>/screens/" title="View and Edit Concerto Screens"><span>Screens</span></a></li>
